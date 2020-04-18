51a03e2e3f4b19f465a04acf4cd65ce9e900a3e3
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-18 15:27:07 +0100

    refactor: SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -10,7 +10,7 @@ class SongList extends React.Component {
           <div className="right floated content">
             <button className="ui button primary" 
               onClick={() => {this.props.selectSong(song)}}
-            >
+            >Select
             </button>
           </div>
           <div className="content">{song.title}</div>

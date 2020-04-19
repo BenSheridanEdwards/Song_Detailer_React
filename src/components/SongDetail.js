b588ce1322cb389cb28e485c3676ce439ee7cba6
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-19 14:58:09 +0100

    feat: display selected song details in segment

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
-  console.log(props)
-  return <div>Song Detail</div>
-};
+const SongDetail = ({ mySelectedSong }) => {
+  if (!mySelectedSong) {
+    return <div className="ui segment">Select a song</div>; 
+  };
+
+  return (
+    <div className="ui segments">
+    <h3 className="ui segment"><u>Song Details:</u></h3>
+    <p className="ui segment"><b>Title:</b>  {mySelectedSong.title}</p>
+    <p className="ui segment"><b>Artist:</b>  {mySelectedSong.artist}</p>
+    <p className="ui segment"><b>Duration:</b>  {mySelectedSong.duration}</p>
+    </div>
+  )};
+
 
 const mapStateToProps = (state) => {
  return { mySelectedSong: state.selectedSong };

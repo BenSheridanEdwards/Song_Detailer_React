8dff02d1e97dfafc7943336c1a433535476fa3e0
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-19 14:59:51 +0100

    style: adding styling to segment and center each song in the list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -6,21 +6,23 @@ class SongList extends React.Component {
   renderList() {
     return this.props.songs.map((song) => {
       return (
+        
         <div className="item" key={song.title}>
           <div className="right floated content">
             <button className="ui button primary" 
               onClick={() => {this.props.selectSong(song)}}
-            >Select
+            >
+            Select
             </button>
           </div>
-          <div className="content">{song.title}</div>
-        </div>
+          <div style={{ display: "flex", paddingTop: 10, alignItems: "center"}} className="content">{song.title}</div>
+          </div>
       );
     });
   }
 
   render () {
-    return <div className="ui divided list">{this.renderList()}</div>;
+    return <div className="ui segment padded divided list">{this.renderList()}</div>;
   }
 }
 

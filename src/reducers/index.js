df94e84886cd24668c23e9d0ca863677f3011c6f
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-19 14:59:02 +0100

    feat: add artists to hard coded songs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux'
 
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', duration: '2:30' },
-    { title: 'All Star', duration: '3: 15' },
-    { title: 'I Want It That Way', duration: '1:45' }
+    { title: 'Forever', artist: 'Chris Brown', duration: '4:05' },
+    { title: 'Low', artist: 'Flo Rida', duration: '3:50' },
+    { title: 'Stronger', artist: 'Kayne West', duration: '5:11' },
+    { title: 'Crank That', artist: 'Soulja Boy', duration: '3:42' }
   ];
 };
 
